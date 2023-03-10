216182d04c53ce828e53b08a4341a019c9248c44
unknown <fveracini@gmail.com>
2023-03-10 15:09:52 +0100

    created the migrations for the invoices

--- database/migrations/2023_03_10_133509_create_invoices_table.php
+++ database/migrations/2023_03_10_133509_create_invoices_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->int('code');
+            $table->decimal('total', 1, 2);
+            $table->enum('payment ', ['Bonifico', 'Carta']);
             $table->timestamps();
         });
     }
