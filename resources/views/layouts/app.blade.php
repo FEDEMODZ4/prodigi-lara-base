0be52c6af9b012ece66cb3c369a9a37682444b27
unknown <fveracini@gmail.com>
2023-03-13 17:22:06 +0100

    add extension for javascript

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
 		<title>@yield('head_title')</title>
 		
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-DOXMLfHhQkvFFp+rWTZwVlPVqdIhpDVYT9csOnHSgWQWPX0v5MCGtjCJbY6ERspU" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.6.4.js"></script>
 	</head>
 
 	<body>
